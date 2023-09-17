528fefe0bf722359dc3623643c7bd269de199ead
Artemiy <timtimk30@yandex.ru>
2023-09-17 22:44:33 +0300

    Minor changes for dihotomy
    Added filtered points for test + small fix

--- SteepestDescent.py
+++ SteepestDescent.py
@@ -1,21 +1,46 @@
 import time
 from datetime import datetime
-
+import numdifftools as nd
 import numpy as np
+def pretty_table(data, cell_sep=' | ', header_separator=True) -> str:
+    rows = len(data)
+    cols = len(data[0])
 
+    col_width = []
+    for col in range(cols):
+        columns = [str(data[row][col]) for row in range(rows)]
+        col_width.append(len(max(columns, key=len)))
+
+    separator = "-+-".join('-' * n for n in col_width)
+
+    lines = []
+
+    for i, row in enumerate(range(rows)):
+        result = []
+        for col in range(cols):
+            item = str(data[row][col]).rjust(col_width[col])
+            result.append(item)
+
+        lines.append(cell_sep.join(result))
+
+        if i == 0 and header_separator:
+            lines.append(separator)
+
+    return '\n'.join(lines)
 start_time = datetime.now()
-st = [-20, 10]
-eposh = 41
-points= np.zeros((eposh, 2))
+st = [-5, -20]
+eposh = 100
+points = np.zeros((eposh, 2))
 points[0] = st
-
-
+accuracy = 0.001
+xscale = 1.5
+yscale = 0.4
 def steepest_descent_with_dichotomy(f, grad_f, initial_x, initial_y, epsilon, max_iterations, alpha_bounds):
     p = [initial_x, initial_y]
     iteration = 1
-
+    numOfFunc = 0
     while iteration < max_iterations:
-        gradient = grad_f(p)
+        gradient = grad_f(f, p)
 
         # Метод дихотомии для определения оптимального шага alpha
         a = alpha_bounds[0]
@@ -28,45 +53,47 @@ def steepest_descent_with_dichotomy(f, grad_f, initial_x, initial_y, epsilon, ma
             else:
                 a = alpha1
             alpha_star = (a + b) / 2
-
+            numOfFunc = numOfFunc + 2
         x_new = p[0] - alpha_star * gradient[0]
         y_new = p[1] - alpha_star * gradient[1]
         if abs(f([x_new, y_new]) - f([p[0], p[1]])) < epsilon:
             break
-
         p = x_new, y_new
         points[iteration] = p
         iteration += 1
-
+    print("iteration: ", iteration)
+    print("numOfFunc: ", numOfFunc)
     return p[0], p[1], iteration
 
 
 def F(x):
-    return 7 * (x[0] + 3) ** 2 + 0.5 * (x[1] + 7) ** 2
+    #return 2 * (x[0] - 1) ** 2 + 10 * (x[1] + 10) ** 2
+    a = x[0] * xscale
+    b = x[1] * yscale
+    return 2 * (a - 1) ** 2 + 30 * (b + 10) ** 2
+
+def grad(func, x):
+    return np.array(nd.Gradient(func)(x))
 
 
-
-def grad(x):
-    return np.array([14 * x[0] + 42, x[1] + 7])
-
-
-result = steepest_descent_with_dichotomy(F, grad, -20, 10, 0.00001, 50, np.array([0.01, 0.4]))
+result = steepest_descent_with_dichotomy(F, grad, st[0], st[1], accuracy, eposh, np.array([0.01, 0.4]))
 print(result)
-print(points)
+#print(points)
 end = datetime.now() - start_time
-print(end)
+#print(end)
 import matplotlib.pyplot as plt
 
-X_estimate, Y_estimate = points[:, 0], points[:, 1]
+filtered_points = points[~np.all(points == 0, axis=1)]
+X_estimate, Y_estimate = filtered_points[:, 0], filtered_points[:, 1]
 # print(X_estimate)
 # print(Y_estimate)
 Z_estimate = F(np.array([X_estimate, Y_estimate]))
 
 min_x0, min_x1 = np.meshgrid(result[0], result[1])
 min_z = F(np.stack([min_x0, min_x1]))
-t = np.linspace(-10, 10, 1000)
+t = np.linspace(-20, 20, 1000)
 X, Y = np.meshgrid(t, t)
-plt.contour(X, Y, F([X, Y]), levels=sorted([F(p) for p in points]))
+plt.contour(X, Y, F([X, Y]), levels=sorted([F(p) for p in filtered_points]))
 plt.plot(X_estimate, Y_estimate, '.-', color="r")
 
 # fig = plt.figure(figsize=(35, 35))
